8c6e16ccac18e4811cbb656076b043d8175415e9
Afekadar <168613463+Afekadar@users.noreply.github.com>
2024-05-06 22:26:53 -0400

    Update Website.jsx

--- src/Components/Pages/Website.jsx
+++ src/Components/Pages/Website.jsx
@@ -4,12 +4,12 @@ import "./page.css";
 export const Website = () => {
   return (
     <div>
-      <Border>This is my Webpage</Border>
+      <Border>This is my Webpage wowwwwwwwwww</Border>
       <div>Some content</div>
     </div>
   );
 };
 
-function Border({children}) {
+function Border({ children }) {
   return <div className="border">{children}</div>;
 }
